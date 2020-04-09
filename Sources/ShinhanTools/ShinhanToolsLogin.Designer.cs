26d17ec7455b7920a57705451a92aca5aa5c233b
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2020-04-09 15:47:35 +0900

    로그인 Window

---
+++ Sources/ShinhanTools/ShinhanToolsLogin.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace ShinhanTools
+{
+    partial class ShinhanToolsLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userId = new System.Windows.Forms.TextBox();
+            this.userPw = new System.Windows.Forms.TextBox();
+            this.userCertiPw = new System.Windows.Forms.TextBox();
+            this.doLoginButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.aboutButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // userId
+            // 
+            this.userId.Location = new System.Drawing.Point(97, 12);
+            this.userId.Name = "userId";
+            this.userId.PasswordChar = '*';
+            this.userId.Size = new System.Drawing.Size(209, 20);
+            this.userId.TabIndex = 0;
+            // 
+            // userPw
+            // 
+            this.userPw.Location = new System.Drawing.Point(97, 38);
+            this.userPw.Name = "userPw";
+            this.userPw.PasswordChar = '*';
+            this.userPw.Size = new System.Drawing.Size(209, 20);
+            this.userPw.TabIndex = 1;
+            // 
+            // userCertiPw
+            // 
+            this.userCertiPw.Location = new System.Drawing.Point(97, 64);
+            this.userCertiPw.Name = "userCertiPw";
+            this.userCertiPw.PasswordChar = '*';
+            this.userCertiPw.Size = new System.Drawing.Size(209, 20);
+            this.userCertiPw.TabIndex = 2;
+            // 
+            // doLoginButton
+            // 
+            this.doLoginButton.Location = new System.Drawing.Point(97, 91);
+            this.doLoginButton.Name = "doLoginButton";
+            this.doLoginButton.Size = new System.Drawing.Size(132, 26);
+            this.doLoginButton.TabIndex = 3;
+            this.doLoginButton.Text = "로그인";
+            this.doLoginButton.UseVisualStyleBackColor = true;
+            this.doLoginButton.Click += new System.EventHandler(this.doLoginButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "사용자ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "사용자PW";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "인증서PW";
+            // 
+            // aboutButton
+            // 
+            this.aboutButton.Location = new System.Drawing.Point(235, 91);
+            this.aboutButton.Name = "aboutButton";
+            this.aboutButton.Size = new System.Drawing.Size(71, 26);
+            this.aboutButton.TabIndex = 4;
+            this.aboutButton.Text = "정보";
+            this.aboutButton.UseVisualStyleBackColor = true;
+            this.aboutButton.Click += new System.EventHandler(this.aboutButton_Click);
+            // 
+            // ShinhanToolsLogin
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(318, 129);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.aboutButton);
+            this.Controls.Add(this.doLoginButton);
+            this.Controls.Add(this.userCertiPw);
+            this.Controls.Add(this.userPw);
+            this.Controls.Add(this.userId);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ShinhanToolsLogin";
+            this.Text = "ShinhanToolsLogin";
+            this.Load += new System.EventHandler(this.ShinhanToolsLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox userId;
+        private System.Windows.Forms.TextBox userPw;
+        private System.Windows.Forms.TextBox userCertiPw;
+        private System.Windows.Forms.Button doLoginButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button aboutButton;
+    }
+}
---
+++ Sources/ShinhanTools/ShinhanToolsLogin.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AxGIEXPERTCONTROLLib;
+using GIEXPERTCONTROLLib;
+
+namespace ShinhanTools
+{
+    public partial class ShinhanToolsLogin : Form
+    {
+        public OpenFileDialog    dialog;
+        public AxGiExpertControl control;
+        public string            indiPath = @"C:\SHINHAN-i\indi\giexpertstarter.exe";
+
+        public ShinhanToolsLogin(ShinhanTools host)
+        {
+            InitializeComponent();
+
+            dialog = new OpenFileDialog
+            {
+                Title  = "Locating Shinhan-Indi Starter",
+                Filter = "Shinhan-Indi Executable|giexpertstarter.exe"
+            };
+
+            control = host.control;
+        }
+
+        private void ShinhanToolsLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void doLoginButton_Click(object sender, EventArgs e)
+        {
+            var id = userId.Text;
+            var pw = userPw.Text;
+            var cp = userCertiPw.Text;
+
+            if (!File.Exists(indiPath) && dialog.ShowDialog() == DialogResult.OK)
+            {
+                indiPath = dialog.FileName;
+            }
+
+
+            if (control.StartIndi(id, pw, cp, indiPath))
+            {
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("로그인 실패!");
+            }
+        }
+
+        private void aboutButton_Click(object sender, EventArgs e)
+        {
+            control.AboutBox();
+        }
+    }
+}
