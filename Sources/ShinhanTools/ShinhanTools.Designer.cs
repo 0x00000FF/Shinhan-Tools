a9dd461fa8dae54f18e934d259b14b0de6694d3d
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2020-04-09 15:48:01 +0900

    메인 Window

--- Sources/ShinhanTools/ShinhanTools.Designer.cs
+++ Sources/ShinhanTools/ShinhanTools.Designer.cs
@@ -28,20 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ShinhanTools));
+            this.control = new AxGIEXPERTCONTROLLib.AxGiExpertControl();
+            ((System.ComponentModel.ISupportInitialize)(this.control)).BeginInit();
             this.SuspendLayout();
+            // 
+            // control
+            // 
+            this.control.Enabled = true;
+            this.control.Location = new System.Drawing.Point(6335, 487);
+            this.control.Name = "control";
+            this.control.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("control.OcxState")));
+            this.control.Size = new System.Drawing.Size(136, 50);
+            this.control.TabIndex = 0;
             // 
             // ShinhanTools
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.control);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "ShinhanTools";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.control)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        internal AxGIEXPERTCONTROLLib.AxGiExpertControl control;
     }
 }
 
--- Sources/ShinhanTools/ShinhanTools.cs
+++ Sources/ShinhanTools/ShinhanTools.cs
@@ -4,22 +4,38 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
-using GIEXPERTCONTROLLib;
 
 namespace ShinhanTools
 {
     public partial class ShinhanTools : Form
     {
-        GiExpertControl control = new GiExpertControl();
+        private bool IsCommOK
+        {
+            get => control.GetCommState() == 0;
+        }
 
         public ShinhanTools()
         {
             InitializeComponent();
-            
+
+            try
+            {
+                var login = new ShinhanToolsLogin(this);
+                login.ShowDialog();
+            }
+            catch (COMException e)
+            {
+                var message = $"Cannot initialize GiExpertControl COM Object\n" +
+                              $"Message: {e.Message}";
+
+                MessageBox.Show(message, "ShinhanTools",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(-1);
+            }
         }
 
     }
